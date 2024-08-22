41bf227966ade00d9f065bdda43b71492eebfc9b
Inishka Mathur <mathurinishka@gmail.com>
2024-08-22 13:23:08 +0530

    Updated code with new features

---
+++ frontend/src/app/page.jsx
@@ -0,0 +1,22 @@
+"use client";
+import { useState } from 'react'
+import Dashboard from "@/components/Dashboard";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
+export default function Home() {
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
+
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle)
+  }
+
+  return (
+    <div className="grid-container">
+      <Navbar OpenSidebar={OpenSidebar}/>
+      <Sidebar openSidebarToggle={openSidebarToggle} OpenSidebar={OpenSidebar}/>
+      <Dashboard />
+    </div>
+  );
+}
