ca10a07051db5c34a5b50675e3c58aa86c452f69
Inishka Mathur <mathurinishka@gmail.com>
2024-08-22 14:19:50 +0530

    Updated code with new features

--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -1,9 +1,11 @@
 "use client";
+
 import { useState } from 'react'
 import Dashboard from "@/components/Dashboard";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import Image from "next/image";
+import "./globals.css";
 
 export default function Home() {
   const [openSidebarToggle, setOpenSidebarToggle] = useState(false)
--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import 
-{ BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillGearFill}
- from 'react-icons/bs'
- import 
- { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line } 
- from 'recharts';
+import { BsFillArchiveFill, BsFillGrid3X3GapFill, BsPeopleFill, BsFillGearFill }
+  from 'react-icons/bs'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, LineChart, Line }
+  from 'recharts';
 import IdentifyVCs from './IdentifyVCs';
 import InvestmentTrends from './InvestmentTrends';
 //import CompetitorAnalysis from './CompetitorAnalysis';
@@ -14,151 +12,160 @@ import Competitors from './Competitors';
 
 function Dashboard() {
 
-    const data = [
-        {
-          name: 'Page A',
-          uv: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
-     
+  const demodata = [
+    {
+      name: 'Page A',
+      uv: 4000,
+      pv: 2400,
+      amt: 2400,
+    },
+    {
+      name: 'Page B',
+      uv: 3000,
+      pv: 1398,
+      amt: 2210,
+    },
+    {
+      name: 'Page C',
+      uv: 2000,
+      pv: 9800,
+      amt: 2290,
+    },
+    {
+      name: 'Page D',
+      uv: 2780,
+      pv: 3908,
+      amt: 2000,
+    },
+    {
+      name: 'Page E',
+      uv: 1890,
+      pv: 4800,
+      amt: 2181,
+    },
+    {
+      name: 'Page F',
+      uv: 2390,
+      pv: 3800,
+      amt: 2500,
+    },
+    {
+      name: 'Page G',
+      uv: 3490,
+      pv: 4300,
+      amt: 2100,
+    },
+  ];
+
 
   return (
     <main className='main-container'>
-        <div className='main-title'>
-            <h3>DASHBOARD</h3>
-        </div>
+      <div className='main-title'>
+        <h3>DASHBOARD</h3>
+      </div>
 
-        <div className='main-cards'>
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>InvestmentTrends</h3>
-                    <BsFillArchiveFill className='card_icon'/>
-                </div>
-                <h1>300</h1>
-            </div>
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>MarketGaps</h3>
-                    <BsFillGearFill className='icon'/>
-                </div>
-                <h1>12</h1>
-            </div>
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>RelatedCompanies</h3>
-                    <BsPeopleFill className='card_icon'/>
-                </div>
-                <h1>33</h1>
-            </div>
-            <div className='card'>
-                <div className='card-inner'>
-                    <h3>CompetitorAnalysis</h3>
-                    <BsFillGrid3X3GapFill className='card_icon'/>
-                    
-                </div>
-                <h1>42</h1>
-            </div>
+      <div className='main-cards'>
+        <div className='card'>
+          <div className='card-inner'>
+            <h3>InvestmentTrends</h3>
+            <BsFillArchiveFill className='card_icon' />
+          </div>
+          <h1>300</h1>
         </div>
+        <div className='card'>
+          <div className='card-inner'>
+            <h3>MarketGaps</h3>
+            <BsFillGearFill className='icon' />
+          </div>
+          <h1>12</h1>
+        </div>
+        <div className='card'>
+          <div className='card-inner'>
+            <h3>RelatedCompanies</h3>
+            <BsPeopleFill className='card_icon' />
+          </div>
+          <h1>33</h1>
+        </div>
+        <div className='card'>
+          <div className='card-inner'>
+            <h3>CompetitorAnalysis</h3>
+            <BsFillGrid3X3GapFill className='card_icon' />
 
-        <div className='charts'>
-            <ResponsiveContainer width="100%" height="100%">
-            <BarChart
+          </div>
+          <h1>42</h1>
+        </div>
+      </div>
+
+      <div className='charts'>
+        <ResponsiveContainer width="100%" height="100%">
+          <BarChart
             width={500}
             height={300}
-            data={data}
+            data={demodata}
             margin={{
-                top: 5,
-                right: 30,
-                left: 20,
-                bottom: 5,
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
             }}
-            >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="pv" fill="#8884d8" />
-                <Bar dataKey="uv" fill="#82ca9d" />
-                </BarChart>
-            </ResponsiveContainer>
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="pv" fill="#8884d8" />
+            <Bar dataKey="uv" fill="#82ca9d" />
+          </BarChart>
+        </ResponsiveContainer>
 
-            <ResponsiveContainer width="100%" height="100%">
-                <LineChart
-                width={500}
-                height={300}
-                data={data}
-                margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
-                }}
-                >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
-                </LineChart>
-            </ResponsiveContainer>
-            <ResponsiveContainer>
-              <IdentifyVCs />
-            </ResponsiveContainer>
-            <ResponsiveContainer>
-              <InvestmentTrends />
-            </ResponsiveContainer>
-            <ResponsiveContainer>
-              <Competitors />
-            </ResponsiveContainer>
-            <ResponsiveContainer>
-              <RelatedCompanies />
-            </ResponsiveContainer>
-            <ResponsiveContainer>
-              <MarketGaps />
-            </ResponsiveContainer>
-
-        </div>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            width={500}
+            height={300}
+            data={demodata}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+            <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+      <div>
+        <ResponsiveContainer>
+          <IdentifyVCs />
+        </ResponsiveContainer>
+      </div>
+      <div>
+        <ResponsiveContainer>
+          <InvestmentTrends />
+        </ResponsiveContainer>
+      </div>
+      <div>
+        <ResponsiveContainer>
+          <Competitors />
+        </ResponsiveContainer>
+      </div>
+      <div>
+        <ResponsiveContainer>
+          <RelatedCompanies />
+        </ResponsiveContainer>
+      </div>
+      <div>
+        <ResponsiveContainer>
+          <MarketGaps />
+        </ResponsiveContainer>
+      </div>
     </main>
   )
 }
